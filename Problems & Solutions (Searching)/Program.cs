61615fdf333797f09e28524eaa90cafff521bc47
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-05-11 21:00:03 +0700

    solve problem 18 (Searching)
    11/5/2024

--- Problems & Solutions (Searching)/Program.cs
+++ Problems & Solutions (Searching)/Program.cs
@@ -51,6 +51,8 @@
     //            }
     //        }
 
+    //results.Add(arr[arr.Length - 1], k);
+
     //        int maxValue = results[arr[0]];
 
     //        foreach (int i in results.Values)
@@ -133,32 +135,117 @@
      *            duplicates in the list. One of the integers is missing in the list. Given an algorithm to find the
      *            missing integer. Example: I/P: [1,2,4,6,3,7,8] O/P: 5
      */
+    //public class Solution
+    //{
+    //    public int FindingMissingNumber(int[] arr)
+    //    {
+    //        Array.Sort(arr);
+
+    //        for(int i = 0; i < arr.Length - 1; i++)
+    //        {
+    //            if (arr[i + 1] - arr[i] == 2)
+    //            {
+    //                return (arr[i] + 1);
+    //            }
+    //        }
+
+    //        return -1;
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Solution solution = new Solution();
+    //        int[] arr = { 1, 2, 4, 6, 3, 7, 8 };
+
+    //        Console.WriteLine(solution.FindingMissingNumber(arr));
+    //    }
+    //}
+    #endregion
+
+    #region Problem 18. Find the Number Occurring an Odd number of times
+    /*
+     * - Problem: Given an array of positive integers, all numbers occur an even number of times except one number which 
+     *            occurs an odd number of times. Find the number in O(n) time & constant space. 
+     *            Example : I/P = [1,2,3,2,3,1,3] O/P = 3
+     */
+    //public class Solution
+    //{
+    //    public int FindingNumberOccurEvenTimes(int[] arr)
+    //    {
+    //        Array.Sort(arr);
+
+    //        Dictionary<int, int> results = new Dictionary<int, int>();
+
+    //        int k = 1;
+
+    //        for (int i = 0; i < arr.Length - 1; i++)
+    //        {
+    //            if (arr[i] == arr[i + 1])
+    //                k++;
+    //            else if (arr[i] != arr[i + 1])
+    //            {
+    //                results.Add(arr[i], k);
+    //                k = 1;
+    //            }
+    //        }
+
+    //        results.Add(arr[arr.Length - 1], k);
+
+    //        foreach (int i in results.Values)
+    //        {
+    //            if (i % 2 != 0)
+    //                return GetKeyFromValue(results, i);
+    //        }
+
+    //        return -1;
+    //    }
+
+    //    private TKey GetKeyFromValue<TKey, TValue>(Dictionary<TKey, TValue> dictionary, TValue value)
+    //    {
+    //        foreach (KeyValuePair<TKey, TValue> pair in dictionary)
+    //        {
+    //            if (pair.Value.Equals(value))
+    //                return pair.Key;
+    //        }
+
+    //        throw new Exception("Value not found in dictionary");
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Solution solution = new Solution();
+    //        int[] arr = { 1, 2, 3, 2, 3, 1, 3 };
+
+    //        Console.WriteLine(solution.FindingNumberOccurEvenTimes(arr));
+    //    }
+    //}
+    #endregion
+
+    #region Problem 19. Find the two repeating elements in a given array
+    /*
+     * - Problem: Given an array with size, all elements of the array are in range 1 to n and also all elements occur 
+     *            only once except two numbers which occur twice. Find those two repeating numbers. 
+     * - For example: if the array is 4,2,4,5,2,3,1 with size = 7 and n = 5. This input has n + 2 = 7 elements with all 
+     *                elements occurring once except 2 and 4 which occur twice. So the output should be 4 2.
+     */
     public class Solution
     {
-        public int FindingMissingNumber(int[] arr)
-        {
-            Array.Sort(arr);
-
-            for(int i = 0; i < arr.Length - 1; i++)
-            {
-                if (arr[i + 1] - arr[i] == 2)
-                {
-                    return (arr[i] + 1);
-                }
-            }
-
-            return -1;
-        }
+        
     }
-
+    
     class MainClass
     {
         static void Main(string[] args)
         {
             Solution solution = new Solution();
-            int[] arr = { 1, 2, 4, 6, 3, 7, 8 };
 
-            Console.WriteLine(solution.FindingMissingNumber(arr));
+
         }
     }
     #endregion
