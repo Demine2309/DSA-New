bd6dce791e070c98b45fd297ae779f1fd8247b7c
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-05-15 10:57:15 +0700

    Practice to interview
    15/5/2024

--- Greedy Algorithm/Program.cs
+++ Greedy Algorithm/Program.cs
@@ -40,230 +40,225 @@ namespace DSA
     //}
     #endregion
 
-
-
     #region The scheduling problem
-    //public class Solution
-    //{
-    //    public double CalculateObjectFunction(int[] P, int[] T, int N)
-    //    {
-    //        double[] S = new double[N]; // Array to store the scores
-    //        double C = 0;               // Completion times
-    //        double F = 0;               // Objective function
-
-    //        for (int i = 0; i < N; i++)
-    //        {
-    //            S[i] = (double)P[i] / T[i];
-    //        }
-
-    //        Array.Sort(S);
-
-    //        for (int i = 0; i < N; ++i)
-    //        {
-    //            int index = Array.IndexOf(S, (double)P[i] / T[i]);
-    //            C += T[index];
-    //            F += P[index] * C;
-    //        }
-
-    //        return F;
-    //    }
-    //}
-    //class MainClass
-    //{
-    //    static void Main(string[] args)
-    //    {
-
-    //        #region Example 2
-    //        Solution solution = new Solution();
-
-    //        int[] P = { 3, 1 };
-    //        int[] T = { 5, 2 };
-    //        int N = P.Length;
-
-    //        double result = solution.CalculateObjectFunction(P, T, N);
-
-    //        Console.WriteLine(result);
-    //        #endregion
-    //    }
-    //}
-    #endregion
-
-    #region Huffman Coding Compression Algorithm
-    public class HuffmanNode
+    public class Solution
     {
-        public char Character { get; set; }
-        public int Frequency { get; set; }
-        public HuffmanNode Left { get; set; }
-        public HuffmanNode Right { get; set; }
-    }
-
-    public class MinHeap
-    {
-        private List<HuffmanNode> heap;
-
-        public MinHeap()
+        public double CalculateObjectFunction(int[] P, int[] T, int N)
         {
-            heap = new List<HuffmanNode>();
-        }
+            double[] S = new double[N]; // Array to store the scores
+            double C = 0;               // Completion times
+            double F = 0;               // Objective function
 
-        public int Size => heap.Count;
-
-        public void Insert(HuffmanNode item)
-        {
-            heap.Add(item);
-            HeapifyUp(heap.Count - 1);
-        }
-
-        public HuffmanNode ExtractMin()
-        {
-            if (heap.Count == 0)
-                throw new InvalidOperationException("Heap is empty.");
-
-            var min = heap[0];
-            heap[0] = heap[heap.Count - 1];
-            heap.RemoveAt(heap.Count - 1);
-            HeapifyDown(0);
-
-            return min;
-        }
-
-        private void HeapifyUp(int index)
-        {
-            while (index > 0)
+            for (int i = 0; i < N; i++)
             {
-                int parent = (index - 1) / 2;
-                if (heap[parent].Frequency > heap[index].Frequency)
-                {
-                    Swap(parent, index);
-                    index = parent;
-                }
-                else
-                {
-                    break;
-                }
+                S[i] = (double)P[i] / T[i];
             }
-        }
 
-        private void HeapifyDown(int index)
-        {
-            int leftChild = 2 * index + 1;
-            int rightChild = 2 * index + 2;
-            int smallest = index;
+            Array.Sort(S);
 
-            if (leftChild < heap.Count && heap[leftChild].Frequency < heap[smallest].Frequency)
-                smallest = leftChild;
-            if (rightChild < heap.Count && heap[rightChild].Frequency < heap[smallest].Frequency)
-                smallest = rightChild;
-
-            if (smallest != index)
+            for (int i = 0; i < N; ++i)
             {
-                Swap(index, smallest);
-                HeapifyDown(smallest);
+                int index = Array.IndexOf(S, (double)P[i] / T[i]);
+                C += T[index];
+                F += P[index] * C;
             }
-        }
 
-        private void Swap(int i, int j)
-        {
-            var temp = heap[i];
-            heap[i] = heap[j];
-            heap[j] = temp;
+            return F;
         }
     }
-
-    public class HuffmanTree
-    {
-        public HuffmanNode Root { get; private set; }
-
-        public HuffmanTree(Dictionary<char, int> charFrequencies)
-        {
-            // Build the Huffman tree
-            var minHeap = new MinHeap();
-
-            foreach (var kvp in charFrequencies)
-            {
-                minHeap.Insert(new HuffmanNode { Character = kvp.Key, Frequency = kvp.Value });
-            }
-
-            while (minHeap.Size > 1)
-            {
-                var left = minHeap.ExtractMin();
-                var right = minHeap.ExtractMin();
-                var newNode = new HuffmanNode
-                {
-                    Frequency = left.Frequency + right.Frequency,
-                    Left = left,
-                    Right = right
-                };
-                minHeap.Insert(newNode);
-            }
-
-            Root = minHeap.ExtractMin();
-        }
-
-        public string Encode(string text)
-        {
-            // Encode the input text using the Huffman tree
-            var encoding = new Dictionary<char, string>();
-            EncodeRecursive(Root, "", encoding);
-            var encodedText = string.Join("", text.ToCharArray().Select(c => encoding[c]));
-            return encodedText;
-        }
-
-        private void EncodeRecursive(HuffmanNode node, string code, Dictionary<char, string> encoding)
-        {
-            if (node.Character != null)
-            {
-                encoding[node.Character] = code;
-            }
-            else
-            {
-                EncodeRecursive(node.Left, code + "0", encoding);
-                EncodeRecursive(node.Right, code + "1", encoding);
-            }
-        }
-
-        public string Decode(string encodedText)
-        {
-            // Decode the input encoded text using the Huffman tree
-            var decodedText = "";
-            var currentNode = Root;
-            foreach (var bit in encodedText)
-            {
-                currentNode = bit == '0' ? currentNode.Left : currentNode.Right;
-                if (currentNode.Character != null)
-                {
-                    decodedText += currentNode.Character;
-                    currentNode = Root;
-                }
-            }
-            return decodedText;
-        }
-    }
-
     class MainClass
     {
         static void Main(string[] args)
         {
-            var inputText = "hello world!";
-            var charFrequencies = new Dictionary<char, int>();
+            Solution solution = new Solution();
 
-            foreach(char c in inputText)
-            {
-                if (charFrequencies.ContainsKey(c))
-                    charFrequencies[c]++;
-                else
-                    charFrequencies[c] = 1;
-            }
+            int[] P = { 3, 1 };
+            int[] T = { 5, 2 };
+            int N = P.Length;
 
-            var huffmanTree = new HuffmanTree(charFrequencies);
-            var encodedText = huffmanTree.Encode(inputText);
-            var decodedText = huffmanTree.Decode(encodedText);
+            double result = solution.CalculateObjectFunction(P, T, N);
 
-            Console.WriteLine($"Input text: {inputText}");
-            Console.WriteLine($"Encoded text: {encodedText}");
-            Console.WriteLine($"Decoded text: {decodedText}");
-
+            Console.WriteLine(result);
         }
     }
+    #endregion
+
+    #region Huffman Coding Compression Algorithm
+    //public class HuffmanNode
+    //{
+    //    public char Character { get; set; }
+    //    public int Frequency { get; set; }
+    //    public HuffmanNode Left { get; set; }
+    //    public HuffmanNode Right { get; set; }
+    //}
+
+    //public class MinHeap
+    //{
+    //    private List<HuffmanNode> heap;
+
+    //    public MinHeap()
+    //    {
+    //        heap = new List<HuffmanNode>();
+    //    }
+
+    //    public int Size => heap.Count;
+
+    //    public void Insert(HuffmanNode item)
+    //    {
+    //        heap.Add(item);
+    //        HeapifyUp(heap.Count - 1);
+    //    }
+
+    //    public HuffmanNode ExtractMin()
+    //    {
+    //        if (heap.Count == 0)
+    //            throw new InvalidOperationException("Heap is empty.");
+
+    //        var min = heap[0];
+    //        heap[0] = heap[heap.Count - 1];
+    //        heap.RemoveAt(heap.Count - 1);
+    //        HeapifyDown(0);
+
+    //        return min;
+    //    }
+
+    //    private void HeapifyUp(int index)
+    //    {
+    //        while (index > 0)
+    //        {
+    //            int parent = (index - 1) / 2;
+    //            if (heap[parent].Frequency > heap[index].Frequency)
+    //            {
+    //                Swap(parent, index);
+    //                index = parent;
+    //            }
+    //            else
+    //            {
+    //                break;
+    //            }
+    //        }
+    //    }
+
+    //    private void HeapifyDown(int index)
+    //    {
+    //        int leftChild = 2 * index + 1;
+    //        int rightChild = 2 * index + 2;
+    //        int smallest = index;
+
+    //        if (leftChild < heap.Count && heap[leftChild].Frequency < heap[smallest].Frequency)
+    //            smallest = leftChild;
+    //        if (rightChild < heap.Count && heap[rightChild].Frequency < heap[smallest].Frequency)
+    //            smallest = rightChild;
+
+    //        if (smallest != index)
+    //        {
+    //            Swap(index, smallest);
+    //            HeapifyDown(smallest);
+    //        }
+    //    }
+
+    //    private void Swap(int i, int j)
+    //    {
+    //        var temp = heap[i];
+    //        heap[i] = heap[j];
+    //        heap[j] = temp;
+    //    }
+    //}
+
+    //public class HuffmanTree
+    //{
+    //    public HuffmanNode Root { get; private set; }
+
+    //    public HuffmanTree(Dictionary<char, int> charFrequencies)
+    //    {
+    //        // Build the Huffman tree
+    //        var minHeap = new MinHeap();
+
+    //        foreach (var kvp in charFrequencies)
+    //        {
+    //            minHeap.Insert(new HuffmanNode { Character = kvp.Key, Frequency = kvp.Value });
+    //        }
+
+    //        while (minHeap.Size > 1)
+    //        {
+    //            var left = minHeap.ExtractMin();
+    //            var right = minHeap.ExtractMin();
+    //            var newNode = new HuffmanNode
+    //            {
+    //                Frequency = left.Frequency + right.Frequency,
+    //                Left = left,
+    //                Right = right
+    //            };
+    //            minHeap.Insert(newNode);
+    //        }
+
+    //        Root = minHeap.ExtractMin();
+    //    }
+
+    //    public string Encode(string text)
+    //    {
+    //        // Encode the input text using the Huffman tree
+    //        var encoding = new Dictionary<char, string>();
+    //        EncodeRecursive(Root, "", encoding);
+    //        var encodedText = string.Join("", text.ToCharArray().Select(c => encoding[c]));
+    //        return encodedText;
+    //    }
+
+    //    private void EncodeRecursive(HuffmanNode node, string code, Dictionary<char, string> encoding)
+    //    {
+    //        if (node.Character != null)
+    //        {
+    //            encoding[node.Character] = code;
+    //        }
+    //        else
+    //        {
+    //            EncodeRecursive(node.Left, code + "0", encoding);
+    //            EncodeRecursive(node.Right, code + "1", encoding);
+    //        }
+    //    }
+
+    //    public string Decode(string encodedText)
+    //    {
+    //        // Decode the input encoded text using the Huffman tree
+    //        var decodedText = "";
+    //        var currentNode = Root;
+    //        foreach (var bit in encodedText)
+    //        {
+    //            currentNode = bit == '0' ? currentNode.Left : currentNode.Right;
+    //            if (currentNode.Character != null)
+    //            {
+    //                decodedText += currentNode.Character;
+    //                currentNode = Root;
+    //            }
+    //        }
+    //        return decodedText;
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var inputText = "hello world!";
+    //        var charFrequencies = new Dictionary<char, int>();
+
+    //        foreach(char c in inputText)
+    //        {
+    //            if (charFrequencies.ContainsKey(c))
+    //                charFrequencies[c]++;
+    //            else
+    //                charFrequencies[c] = 1;
+    //        }
+
+    //        var huffmanTree = new HuffmanTree(charFrequencies);
+    //        var encodedText = huffmanTree.Encode(inputText);
+    //        var decodedText = huffmanTree.Decode(encodedText);
+
+    //        Console.WriteLine($"Input text: {inputText}");
+    //        Console.WriteLine($"Encoded text: {encodedText}");
+    //        Console.WriteLine($"Decoded text: {decodedText}");
+
+    //    }
+    //}
     #endregion
 }
--- Problems & Solutions (Greedy Algorithms)/Program.cs
+++ Problems & Solutions (Greedy Algorithms)/Program.cs
@@ -1,6 +1,124 @@
 ﻿namespace DSA
 {
-	#region Problem 1
+    #region Problem 1
+    /*
+	 * - Problem: Given an array F with size n. Assume the array content F[i] indicates the length of the ith 
+	 *            file and we want to merge all these files into one single file. Check whether the following 
+	 *            algorithm gives the best solution for this problem or not?
+	 * - Algorithm: Merge the files contiguously (liền kề nhau). That means select the first two files and merge
+	 *				them. Then select the output of the previous merge and merge with the third file, and keep 
+	 *				going...
+	 * - NOTE: Given two files A and B with sizes m and n, the complexity of merging is O(m + n).
+	 */
 
-	#endregion
+    /*
+	 * - Solution: This algorithm will not produce the optimal solution
+	 *		+ Assume we have input: F = {10,5,100,50,20,15} 
+	 *		+ The total cost of merging = Cost of all merging operations = 15 + 115 + 165 + 185 + 200 = 680.
+	 *		+ If we sort the input after merge: F = {5,10,15,20,50,100}
+	 *		+ the total cost of merging = 15 + 30 + 50 + 100 + 200 = 395 
+	 */
+    #endregion
+
+    #region Problem 2
+    /*
+	 * - Problem: Similar to Problem-1, does the following algorithm give the optimal solution?
+	 * - Algorithm: Merge the files in pairs. That means after the first step, the algorithm produces the n/2 
+	 *				intermediate files. For the next step, we need to consider these intermediate files and 
+	 *				merge them in pairs and keep going.
+	 * - NOTE: Sometimes this algorithm is called 2-way merging. Instead of two files at a time, if we merge 
+	 *		   K files at a time then we call it K-way merging.
+	 */
+
+    /*
+	 * - Solution: This algorithm will not produce the optimal solution
+	 *		+ Assume we have input: F = {10,5,100,50,20,15} 
+	 *		+ we need to merge the first pair of files (10 and 5 size files), the second pair of files (100 and 50) 
+	 *		  and the third pair of files (20 and 15). We have result {15, 150, 35}
+	 *		+ Iteratve this process: {165, 35}, {185}.
+	 *		+ The total cost of merging = Cost of all merging operations = 15 + 150 + 35 + 165 + 185 = 550 > 395 
+	 *		(in previous case)
+	 */
+    #endregion
+
+    #region Problem 3
+    /*
+	 * - Problem: In Problem-1, what is the best way to merge all the files into a single files?
+	 */
+
+    /*
+	 * - Algorithm: 1. Store file sizes in a priority queue. The key of elements are file lengths.
+	 *				2. Repeat the following until there is only one file:
+	 *					a. Extract two smallest elements X and Y
+	 *					b. Merge X and Y and insert this new file in the priority queue
+	 * - Variant of same algorithm: 1. Sort the file sizes in ascending order.
+	 *								2. Repeat the following until there is only one file:
+	 *									a. Take the first two elements (smallest) X and Y.
+	 *									b. Merge X and Y and insert this new file in the sorted list.
+	 */
+
+    //public class Solution
+    //{
+    //	public int MergeFiles(int[] F)
+    //	{
+    //           var priorityQueue = new SortedSet<(int, int)>();
+    //           for (int i = 0; i < F.Length; i++)
+    //           {
+    //               priorityQueue.Add((F[i], i));
+    //           }
+
+    //           int totalCost = 0;
+
+    //           // Repeat until there is only one file
+    //           while (priorityQueue.Count > 1)
+    //           {
+    //               // Extract two smallest elements X and Y
+    //               var smallest = priorityQueue.Min;
+    //               priorityQueue.Remove(smallest);
+    //               var secondSmallest = priorityQueue.Min;
+    //               priorityQueue.Remove(secondSmallest);
+
+    //               // Merge X and Y and insert this new file in the priority queue
+    //               int mergedSize = smallest.Item1 + secondSmallest.Item1;
+    //               totalCost += mergedSize;
+    //               priorityQueue.Add((mergedSize, smallest.Item2));
+    //           }
+
+    //           return totalCost;
+    //       }
+    //}
+
+    //class MainClass
+    //{
+    //	static void Main(string[] args)
+    //	{
+    //		Solution solution = new Solution();
+    //		int[] F = { 10, 5, 100, 50, 20, 15 };
+
+    //		Console.WriteLine(solution.MergeFiles(F));
+    //	}
+    //}
+    #endregion
+
+    #region Problem 4. Interval Scheduling Algorithm
+    /*
+     * - Problem: Given a set of n intervals S = {(start.i, end.j)|1 ≤ i ≤ n}. Let us assume that we want to find 
+     *            a maximum subset S′ of S such that no pair of intervals in S′ overlaps. Check whether the following
+     *            algorithm works or not.
+     */
+    public class Solution
+    {
+
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+    #endregion
 }
